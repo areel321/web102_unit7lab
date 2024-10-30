f1615eb8b27150c3b80148f93ce722a97f995b7c
areel321 <112980267+areel321@users.noreply.github.com>
2024-10-29 21:20:48 -0400

    dded files

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './EditPost.css'
+import { useState } from 'react';
+import { supabase } from '../client';
 
 const EditPost = ({data}) => {
 
@@ -15,6 +17,27 @@ const EditPost = ({data}) => {
                 [name]:value,
             }
         })
+    }
+
+    const updatePost = async (event) => {
+        event.preventDefault();
+        await supabase
+            .from('Posts')
+            .update({ title: post.title, author: post.author,  description: post.description})
+            .eq('id', id);
+
+        window.location = "/";
+    }
+
+    const deletePost = async (event) => {
+        event.preventDefault();
+       
+        await supabase
+            .from('Posts')
+            .delete()
+            .eq('id', id);
+
+            window.location = "http://localhost:3000/";
     }
 
     return (
@@ -32,8 +55,8 @@ const EditPost = ({data}) => {
                 <textarea rows="5" cols="50" id="description" value={post.description} onChange={handleChange} >
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
+                <input type="submit" value="Submit" onClick={updatePost}/>
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
